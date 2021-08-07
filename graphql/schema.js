c20f68572f55053d4387115f1b7943d17d1c313d
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2021-08-07 14:52:25 +0700

    add schema auth and post graphql

--- graphql/schema.js
+++ graphql/schema.js
@@ -2,16 +2,50 @@ const  { buildSchema } = require('graphql')
 
 module.exports = buildSchema(`
 
-    type TestData {
-        value: String!,
-        room: Int!
+    type Post {
+        _id: ID!
+        title: String!
+        content: String!
+        imageUrl: String!
+    }
+
+    type User {
+        _id: ID!
+        email: String!
+        name: String!
+        password: String!
+        posts: [Post!]
+        status: String!
+    }
+
+    type AuthData {
+        token: String!
+        userId: String!
+    }
+
+    input UserInput {
+        email: String!
+        password: String!
+        name: String!
+    }
+
+    input PostInput {
+        title: String!
+        imageUrl: String!
+        content: String!
     }
 
     type RootQuery {
-        hello: TestData
+        login(email: String!, password: String!): AuthData!
+    }
+
+    type RootMutation {
+        createUser(userInput: UserInput!): User!
+        createPost(postInput: PostInput!): Post! 
     }
 
     schema {
         query: RootQuery
+        mutation: RootMutation
     }
 `)
